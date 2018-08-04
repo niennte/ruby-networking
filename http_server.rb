a43c6f81c5c6d712ed0ec40ab2772b1c786e945a
Niennte <niennte@gmail.com>
2018-08-04 13:52:00 -0400

    Keep listening and update HTTP response.

--- http_server.rb
+++ http_server.rb
@@ -1,18 +1,21 @@
 require 'socket'
 server = TCPServer.new 3001
 
-session = server.accept
-while (request = session.gets) && (request.chomp.length > 0)
-  puts "They said \"#{request.chomp}\""
-end
+loop do
+  session = server.accept
 
-puts "finished reading"
+  while (request = session.gets) && (request.chomp.length > 0)
+    puts "They said \"#{request.chomp}\""
+  end
+
+  puts "finished reading"
 
 my_response = <<-HEREDOC
 HTTP/1.1 200 OK
 
-hello world
+hello world it's #{Time.now.strftime "%H: %M: %S:(%L)"}
 HEREDOC
 
-session.puts my_response
-session.close
+  session.puts my_response
+  session.close
+end
